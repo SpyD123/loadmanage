1ede414c13f46e5d4c44a1d4877a3539c36417c1
spyd123 <chaitanya.n14@iiits.in>
2017-03-02 11:18:36 +0530

    Uploading Backend

--- load/models.py
+++ load/models.py
@@ -5,9 +5,21 @@ TITLE_CHOICES = (
     ('D', 'Done'),
 )
 
+class ApplianceName(models.Model):
+    appliance = models.CharField(max_length=100)
+    def __str__(self):
+        return self.appliance
+
+class Usage(models.Model):
+    app_id=models.ForeignKey(ApplianceName, on_delete=models.CASCADE)
+    use=models.IntegerField(max_length=10,default=0)
+    recordtime=models.DateTimeField()
+    def __str__(self):
+        return str(self.app_id)+" - "+str(self.use)+" - "+str(self.recordtime)
+
 class Complaint(models.Model):
-    num = models.IntegerField(max_length=10,default=1)
+    complaint_num = models.IntegerField(max_length=10,default=1)
     comp = models.CharField(max_length=100)
-    status = models.CharField(max_length=10,choices=TITLE_CHOICES)
+    status = models.CharField(max_length=10,choices=TITLE_CHOICES,default='ND')
     def __str__(self):
         return self.comp+" - "+self.status
